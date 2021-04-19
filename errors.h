abaf149e65186be23aad8160384d3a9c8630aa75
vnechit <vnechit310301@gmail.com>
2021-04-19 21:00:23 +0300

    errors.h done

--- errors.h
+++ errors.h
@@ -1,3 +1,12 @@
 //
 // Created by   User on 19.04.2021.
 //
+
+#pragma once
+#include <stdexcept>
+#include <string>
+
+inline void error(const std::string& msg)
+{
+    throw std::runtime_error(msg);
+}
